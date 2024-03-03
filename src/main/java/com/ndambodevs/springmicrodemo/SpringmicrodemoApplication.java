1a1ed42df089fb98ce72d5f0d4f7fa6c1ecdfd54
ndambodavid <linmik420@gmail.com>
2024-03-03 20:50:24 +0300

    initial commit

---
+++ src/main/java/com/ndambodevs/springmicrodemo/SpringmicrodemoApplication.java
@@ -0,0 +1,13 @@
+package com.ndambodevs.springmicrodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicrodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmicrodemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ndambodevs/springmicrodemo/SpringmicrodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndambodevs.springmicrodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicrodemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
